23a69d5520f630c65725b4ef3cb147e36520622f
Yoon SeungHyun <dbstmdgus5050@gmail.com>
2023-07-17 20:19:11 +0900

    c + v

--- src/main/java/com/iteratrlearning/shu_book/chapter_02/BankStatementAnalyzerSimple.java
+++ src/main/java/com/iteratrlearning/shu_book/chapter_02/BankStatementAnalyzerSimple.java
@@ -16,16 +16,32 @@ public class BankStatementAnalyzerSimple {
     private static final String RESOURCES = "src/main/resources/";
 
     public static void main(final String[] args) throws Exception {
-            final Path path = Paths.get(RESOURCES + "bank-data-simple.csv");
-            final List<String> lines = Files.readAllLines(path);
-            double total = 0;
+            final Path path = Paths.get(RESOURCES + args[0]);
+            // 파일 시스템의 경로 클래스
+            final List<String> lines = Files.readAllLines(path); // 행 목록 반환
+            /* 파일의 모든 행을 가져온 다음, 각 행에 다음 작업을 수행
+            - 콤마로 열 분리
+            - 금액 추출
+            - 금액을 double로 파싱
+            최종적으로 전체 금액의 합계를 얻음
+
+            ??
+            - 파일이 비어있다면?
+            - 데이터에 문제가 있어서 금액을 파싱하지 못한다면?
+            - 행의 데이터가 완벽하지 않다면?
+            */
+            double total = 0d;
+            final DateTimeFormatter DATE_PATTERN = DateTimeFormatter.ofPattern("dd-MM-yyyy");
             for(final String line: lines) {
-                String[] columns = line.split(",");
-                double amount = Double.parseDouble(columns[1]);
-                total += amount;
+                final String[] columns = line.split(",");
+                final LocalDate date = LocalDate.parse(columns[0],DATE_PATTERN);
+                if(date.getMonth() == Month.JANUARY) {
+                    final double amount = Double.parseDouble(columns[1]);
+                    total += amount;
+                }
             }
 
-            System.out.println("The total for all transactions is " + total);
+            System.out.println("The total for all transactions in January is " + total);
     }
 }
 
